213a40aeb800e90d782d3f3a653a7f4b33327c7d
rawat-shashank <shashank17nov@gmail.com>
2022-03-10 22:23:57 +0530

    LC - strings - Easy 1

---
+++ leetcode/strings/1941 - Check if All Characters Have Equal Number of Occurrences.py
@@ -0,0 +1,29 @@
+# Easy - 1941 - Check if All Characters Have Equal Number of Occurrences
+
+import unittest
+
+def firstTry(s):
+    dp = {}
+    for x in s:
+        dp[x] = dp.get(x, 0) + 1
+    res = []
+    for v in dp.values():
+        if v not in res:
+            res.append(v)
+    return len(res) == 1
+
+
+def caller(s):
+    return firstTry(s)
+
+class Tests(unittest.TestCase):
+    def tests(self):
+        self.assertEqual(True, caller(
+            s = "abacbc"
+        ))
+        self.assertEqual(False, caller(
+            s = "aaabb"
+        ))
+
+if __name__ == '__main__':
+    unittest.main()
